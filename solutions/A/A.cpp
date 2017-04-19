4b954b94115a52195447bb9471e926565d649c45
larry98 <larry98@users.noreply.github.com>
2017-04-19 15:09:31 -0400

    fixed io

--- solutions/A/A.cpp
+++ solutions/A/A.cpp
@@ -1,22 +1,18 @@
-#include <fstream>
+#include <iostream>
 
 using namespace std;
 
-int main() {
-  ifstream fin ("A.in");
-  ofstream fout ("A.out")
-  
+int main() { 
   int n, t;
-  fin >> n >> t;
+  cin >> n >> t;
   for (int i = 0; i < n; i++) {
     int a, b;
-    fin >> a >> b;
+    cin >> a >> b;
     if (a / 100.0 * t < b * 1.0)
-      fout << "Once you quit track you can never come back" << endl;
+      cout << "Once you quit track you can never come back" << endl;
     else
-      fout << "What do you mean I'm slow" << endl;
+      cout << "What do you mean I'm slow" << endl;
   }
-  fout.close();
   
   return 0;
 }
