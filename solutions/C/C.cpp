66ef43202441b81ed1fff4c7c10f2f152312c0a9
justinz2018 <justinz2018@users.noreply.github.com>
2017-04-19 10:30:36 -0400

    Create C.cpp

---
+++ solutions/C/C.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  for(int x=0; x<N; x++) {
+    if(x*(x+1)/2 >= N) {
+      cout << i;
+      break;
+    }
+  }
+}
