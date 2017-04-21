165e3beba9f482651ed7123307a126cc77f3abc7
Srinidhi Krishnamurthy <srinidhi0912@gmail.com>
2017-04-21 14:12:09 -0400

    F sol

---
+++ solutions/F/F.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class F{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        HashMap<String, Integer> candies = new HashMap<String, Integer>();
+        for (int i = 0; i < N; i++) {
+            candies.put(sc.next(), sc.nextInt());
+        }
+        String rec = sc.next();
+        String str = "";
+        int count = 0;
+        for(int i = 0; i < rec.length(); i++){
+            str += rec.charAt(i);
+            if(candies.get(str) != null){
+                count += candies.get(str);
+                str = "";
+            }
+        }
+        System.out.println(count);
+    }
+}
