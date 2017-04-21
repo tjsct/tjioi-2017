95b3fc993c1965650a1962b374922d47e468b98b
Alex Wang <alex3763@gmail.com>
2017-04-21 09:38:33 -0400

    fix E

--- solutions/E/E.py
+++ solutions/E/E.py
@@ -7,32 +7,33 @@ pairs = [[] for i in range(N)]
 remaining = set(range(1,N))
 
 for i in range(K):
-	a,b = map(int, input().split())
-	pairs[a].append(b)
-	pairs[b].append(a)
+    a,b = map(int, input().split())
+    pairs[a].append(b)
+    pairs[b].append(a)
 
 ans = 0
 
 def genPerm(perm,left,ind):
-	global ans
-	if len(left) == 0: #base case
-		ans += checkPerm(perm)
-		return
-	for i in left: #recur
-		perm[ind+1] = i
-		left.remove(i)
-		genPerm(perm,left,ind+1)
-		left.add(i)
-		perm[ind+1] = -1
+    global ans
+    if len(left) == 0: #base case
+        ans += checkPerm(perm,ind)
+        return
+    for i in left: #recur
+        perm[ind+1] = i
+        left.remove(i)
+        if checkPerm(perm,ind) == 1:
+            genPerm(perm,left,ind+1)
+        left.add(i)
+        perm[ind+1] = -1
 
-def checkPerm(perm):
-	for i in range(N):
-		nextInd = i+1
-		if nextInd >= N:
-			nextInd = 0
-		if perm[nextInd] in pairs[perm[i]]:
-			return 0
-	return 1
+def checkPerm(perm,length):
+    for i in range(N):
+        nextInd = i+1
+        if nextInd >= N:
+            nextInd = 0
+        if perm[nextInd] in pairs[perm[i]]:
+            return 0
+        return 1
 
 genPerm(circle,remaining,0)
 print(ans)
