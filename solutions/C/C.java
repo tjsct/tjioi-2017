f680ca9561a8cab3008fc35fee3386666dbec4ef
Srinidhi Krishnamurthy <srinidhi0912@gmail.com>
2017-04-21 12:59:44 -0400

    C sol

---
+++ solutions/C/C.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class C{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i = 0; i <= N; i++){
+            if(N <= i*(i+1)/2){
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
