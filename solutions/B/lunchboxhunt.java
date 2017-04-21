7e1fa1aacc80d68ad333e895424d058afff6aead
Srinidhi Krishnamurthy <srinidhi0912@gmail.com>
2017-04-21 12:50:20 -0400

    B sol

---
+++ solutions/B/lunchboxhunt.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class lunchboxhunt{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int x_0 = sc.nextInt();
+        int y_0 = sc.nextInt();
+        for(int i = 0; i < N; i++){
+            String query_direction = sc.next();
+            int query_distance = sc.nextInt();
+            if(query_direction.equals("N")){
+                y_0 += query_distance;
+            }
+            if(query_direction.equals("S")){
+                y_0 -= query_distance; 
+            }
+            if(query_direction.equals("E")){
+                x_0 += query_distance;
+            }
+            if(query_direction.equals("W")){
+                x_0 -= query_distance;
+            }
+        }
+        System.out.println(x_0 + " " + y_0);
+    }
+}
