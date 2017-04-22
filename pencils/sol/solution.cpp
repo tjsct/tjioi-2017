3399ebcc4c5b332a27eb209979669787e1b4d3e9
Kevin Geng <gengkev@gmail.com>
2017-04-22 03:55:11 -0400

    fix solution.cpp for pencils (lol larry)

--- pencils/sol/solution.cpp
+++ pencils/sol/solution.cpp
@@ -1,18 +1,15 @@
-#include <fstream>
+#include <iostream>
 #include <vector>
 #include <algorithm>
 
-using mamespace std;
+using namespace std;
 
 int main() {
-  ifstream fin ("D.in");
-  ofstream fout("D.out");
-  
   int n, k, temp;
   vector<int> pencils;
-  fin >> n >> k;
+  cin >> n >> k;
   for (int i = 0; i < n; i++) {
-    fin >> temp;
+    cin >> temp;
     pencils.push_back(temp);
   }
   sort(pencils.begin(), pencils.end());
@@ -21,8 +18,7 @@ int main() {
   for (int i = n-1; i >= n-k; i--)
     sum += pencils[i];
   
-  fout << sum << endl;
-  fout.close()
-  
+  cout << sum << endl;
+ 
   return 0;
 }
