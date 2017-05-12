aad90fd167765d9cff22cff424ef959337034829
Kevin Geng <gengkev@gmail.com>
2017-05-12 12:11:42 -0400

    redo cookie

---
+++ contest/cookie/gen.sh
@@ -0,0 +1,10 @@
+/usr/bin/python3 gen.py 10 10 1000 > input/input1.txt
+/usr/bin/python3 gen.py 100 1000 10000 > input/input2.txt
+/usr/bin/python3 gen.py 1000 1000 10000 > input/input3.txt
+/usr/bin/python3 gen.py 10000 1000 10000 > input/input4.txt
+/usr/bin/python3 gen.py 10000 10000 1000000 > input/input5.txt
+/usr/bin/python3 gen.py 100000 100000 100000 > input/input6.txt
+/usr/bin/python3 gen.py 10000 100000 1000000 > input/input7.txt
+/usr/bin/python3 gen.py 100000 100000 1000000 > input/input8.txt
+/usr/bin/python3 gen.py 100000 100000 1000000 > input/input9.txt
+
--- contest/cookie/sol/solution.cpp
+++ contest/cookie/sol/solution.cpp
@@ -3,8 +3,8 @@
 #include <set>
 using namespace std;
 
-set<int> st[100005];
-int a[100005];
+set<int> st[1000005];
+int a[1000005];
 
 int main(){
     int N; cin >> N;
@@ -25,48 +25,12 @@ int main(){
         }
     }
     for (int i = 0; i < Q; i++){
-        char c; cin >> c;
-        if (c == 'T'){
-            int u; cin >> u;
-            if (st[u].empty()){
-                cout << -1 << endl;
-            }
-            else{
-                cout << *(st[u].begin()) + 1 << endl;
-            }
+        int u; cin >> u;
+        if (st[u].empty()){
+            cout << -1 << endl;
         }
-        if (c == 'S'){
-            int u, v; cin >> u >> v;
-            u--; v--;
-            int tmp = a[v];
-            a[v] = a[u];
-            a[u] = tmp;
-            int reduced_a_u = a[u]/(__gcd(a[u], a[v]));
-            int reduced_a_v = a[v]/(__gcd(a[u], a[v]));
-            for (int j = 2; j < 400; j++){
-                if (reduced_a_u % j == 0){
-                    while (reduced_a_u % j == 0){
-                        reduced_a_u /= j;
-                    }
-                    st[j].erase(v);
-                    st[j].insert(u);
-                }
-                if (reduced_a_v % j == 0){
-                    while (reduced_a_v % j == 0){
-                        reduced_a_v /= j;
-                    }
-                    st[j].erase(u);
-                    st[j].insert(v);
-                }
-            }
-            if (reduced_a_u > 1){
-                st[reduced_a_u].erase(v);
-                st[reduced_a_u].insert(u);
-            }
-            if (reduced_a_v > 1){
-                st[reduced_a_v].erase(u);
-                st[reduced_a_v].insert(v);
-            }
+        else {
+            cout << *(st[u].begin()) + 1 << endl;
         }
     }
 }
